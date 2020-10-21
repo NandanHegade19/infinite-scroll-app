377a0ed48bf060c94fcedcebed2e29ef7aea8a34
NandanHegade19 <nandanhegade22@gmail.com>
2020-10-21 19:51:15 -0400

    v1 code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [boxes, setBoxes] = useState([{ bg: "#777" }]) //initial box is grey, change it using below random
+
+  useEffect(() => {
+    //stop at fixed no of boxes? use the IF below
+    //if(boxes.length < 21){
+      const interval = setInterval (() => {
+        setBoxes([...boxes, {bg: "#"+((Math.random() * 0xffffff) << 0).toString(16)}])
+      }, 100)
+      return () => clearInterval(interval)
+    //}
+  }, [boxes])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {
+        boxes.map((box, index) => (
+          <div key = {index} className = "box" style = {{background: box.bg}}>
+            <p>{index}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
